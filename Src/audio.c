LiteracyBridge/tbv2

commit c5b3607141fbe287a5d061756ce43d20079ec500

Various fixes and tweaks.
- Changes to boot key detection in main. Plus + Tree-Table goes directly to
  USB mode.
- Disable interrupts on all but Home when sleeping. Only Home wakes device
  (Tree-Table boots, of course).
- Don't send key events when "bootToUSB" is true -- the CSM isn't handling
  those events and the queue overflows and errs out.
- Commented out some problematic lines from control_def.txt (unpublished
  updates to CSMCompile?)
- Added "To listen to another message..." after playing content messages.

Src/audio.c
@@ -576,6 +576,7 @@ void								audLoadBuffs(){																// called on mediaThread to preload a
 }
 
 extern bool BootVerboseLog;
+
 void 								audPlaybackComplete( void ) {									// shut down after completed playback
   int pct;
   haltPlayback();     // updates tsPause & msPlayed
@@ -585,15 +586,15 @@ void 								audPlaybackComplete( void ) {									// shut down after completed 
       pSt.tunePlayed += pSt.nSamples;
   #endif
 
-  ledFg( NULL );				// Turn off foreground LED: no longer playing
   #ifdef _SQUARE_WAVE_SIMULATOR
   if ( pSt.SqrWAVE ){
-	int msTunePlayed = pSt.tsPause - pSt.tsTuneStart;
-	pct = msTunePlayed * 100 / pSt.tuneMsec;
-	logEvtNININI( "tuneDn", "ms", pSt.msPlayed, "pct", pct, "nS", pSt.tunePlayed );
+        int msTunePlayed = pSt.tsPause - pSt.tsTuneStart;
+        pct = msTunePlayed * 100 / pSt.tuneMsec;
+        logEvtNININI( "tuneDn", "ms", pSt.msPlayed, "pct", pct, "nS", pSt.tunePlayed );
   } else
   #endif
   {
+      ledFg( NULL );				// Turn off foreground LED: no longer playing
        pct = 100; //pSt.msPlayed * 100 / pSt.msecLength;
       // complete, so msPlayed == msecLength
       dbgEvt( TB_audDone, pSt.msPlayed, pct, pSt.msPlayed, pSt.msecLength );
@@ -604,9 +605,9 @@ void 								audPlaybackComplete( void ) {									// shut down after completed 
       
       // playback completed, report difference between measured & expected elapsed time
       if ( BootVerboseLog ) logEvtNI("Play_Tim", "dif_ms", pSt.msecLength - pSt.msPlayed );
+      sendEvent( AudioDone, pct );				// end of file playback-- generate CSM event
+      pSt.stats->Finish++;
   }
-  sendEvent( AudioDone, pct );				// end of file playback-- generate CSM event
-  pSt.stats->Finish++;
 }
 
 
@@ -834,7 +835,6 @@ static void 				haltRecord( void ){														// ISR callable: stop audio inp
   pSt.msRecorded += (pSt.tsPause - pSt.tsResume);  		// (tsResume == tsRecord, if never paused)
   dbgEvt( TB_audRecDn, pSt.msRecorded, 0, 0,0 );
 
-  ledFg( NULL );		// cancel fgRecording
   ledFg( TB_Config->fgSavingRec );				// Switch foreground LED to saving
 }
 

Src/USBD_User_Device_MSC_LUN.c
@@ -255,7 +255,6 @@ void	 	USBD_MSC0_Initialize( void ){										// callback from USBD_Initialize t
   for ( int i=0; i<nLUNs; i++ )
 		initLUN( i );
 	usbProvidingMassStorage = true;
-	ledFg( NULL );		// stop USBconnect
 	ledFg( TB_Config->fgUSB_MSC );			// connected
 }
 void 		USBD_MSC0_Uninitialize( void ){									// callback from USBD_Uninitialize to de-init all Logical Units of the USB MSC class instance.

Src/build_version.h
@@ -1,1 +1,1 @@
-#define BUILD_VERSION "V3.1 of 2022-05-07 14:27:15"
+#define BUILD_VERSION "V3.1 of 2022-05-08 18:38:57"

Src/controlmanager.c
@@ -15,6 +15,8 @@
 
 // TBook Control State Machine
 //extern int 	 								nCSMstates;			// defined in tbook_csm.c
+bool controlManagerReady = false;
+
 
 int				    iPkg = 0;			// current package index 
 Package_t *         currPkg = NULL;	// TBook content package in use
@@ -493,6 +495,7 @@ static void						tbTimer( void * eNum ){
 void 									executeCSM( void ){								// execute TBook control state machine
 	TB_Event *evt;
 	osStatus_t status;
+  controlManagerReady = true;
 	
 	TBook.volume = TB_Config->default_volume;
 	TBook.iSubj = -1; // makes "next subject" go to the first subject.

Src/inc/inputMgr.h
@@ -6,7 +6,19 @@
 // TalkingBook keypad has 10 keys, each connected to a line of the keypad cable
 //  plus kINVALID for no key value, & kTIMER for long press timer elapsed
 typedef enum {	kHOME=0, kCIRCLE, kPLUS, kMINUS, kTREE, kLHAND, kRHAND, kPOT, kSTAR, kTABLE, kINVALID, kTIMER } KEY;
-
+// Mask bits for keys, so they can be or'd together.
+typedef enum KEYS_MASK {
+    KM_HOME = 1 << kHOME,
+    KM_CIRCLE = 1 << kCIRCLE,
+    KM_PLUS = 1 << kPLUS,
+    KM_MINUS = 1 << kMINUS,
+    KM_TREE = 1 << kTREE,
+    KM_LHAND = 1 << kLHAND,
+    KM_RHAND = 1 << kRHAND,
+    KM_POT = 1 << kPOT,
+    KM_STAR = 1 << kSTAR,
+    KM_TABLE = 1 << kTABLE
+} KEYS_MASK_T;
 typedef struct	{	// KeyPadKey -- assignments to GPIO pins, in inputManager.cpp
 	GPIO_ID				id;			// idx in gpio_def
 	KEY						key;		// KEYPAD key connected to this line
@@ -23,6 +35,8 @@ typedef struct	{	// KeyPadKey -- assignments to GPIO pins, in inputManager.cpp
 	uint32_t			dntime;		// num tbTicks it was down
 
 } KeyPadKey_t;
+typedef KeyPadKey_t 	 KeyPadKey_arr[ 10 ];		// so Dbg knows array size
+extern KeyPadKey_t keydef[10];
 
 /*typedef struct { 	// KeyState --	to keep track of the state of a keypad GPIO line-- SET/RESET and time of last change
 	KEY					key;
@@ -64,6 +78,7 @@ inline Event 	toLongEvt(  KEY k ){	return (Event)( (int)k + Home__ );  }
 inline Event 	toStarEvt(  KEY k ){	return (Event)( (int)k + starHome );  }
 
 extern void 	initInputManager( void );
+extern void   disableKeyInterrupts( enum KEYS_MASK keysToRemainEnabled );
 extern void 	keyPadPowerDown( void );						// shut down keypad GPIOs
 extern void 	keyPadPowerUp( void );							// re-initialize keypad GPIOs
 extern void 	sendEvent( Event, int32_t );

Src/inc/tbook.h
@@ -34,6 +34,17 @@
 #include "powermanager.h"      // registerPowerEventHandler
 #include "inputMgr.h"			// sendEvent
 
+// global state
+extern bool BootToUSB;
+extern bool BootDebugLoop;
+extern bool BootVerboseLog;
+extern bool BootToQCtest;
+extern bool BootVerbosePower;
+extern bool BootResetLog;
+extern bool BootFormatFileSys;
+
+extern bool controlManagerReady;
+
 
 // global utilities
 // tbutil.c
@@ -257,7 +268,6 @@ typedef  struct {
 	KEY 					LongPressKey;			// key reported as long press -- shared ISR & thread
 */
 } KeyPadState_t;
-typedef KeyPadKey_t 	 KeyPadKey_arr[ NUM_KEYPADS ];		// so Dbg knows array size
 
 typedef struct {
 	bool 				Active;

Src/inputmanager.c
@@ -56,7 +56,7 @@ exti10_15 PA15  gTREE     8000
 EXTI_IMR                  87BB
 */
 
-KeyPadKey_arr 			keydef = {  // keypad GPIO in order: kHOME, kCIRCLE, kPLUS, kMINUS, kTREE, kLHAND, kPOT, kRHAND, kSTAR, kTABLE
+KeyPadKey_t keydef[10] = {  // keypad GPIO in order: kHOME, kCIRCLE, kPLUS, kMINUS, kTREE, kLHAND, kPOT, kRHAND, kSTAR, kTABLE
 // static placeholders, filled in by initializeInterrupts
 //	GPIO_ID    key,    port,  	pin,         intq				extiBit		signal		pressed	 down   tstamp dntime
 	{  gHOME,		kHOME,	  NULL, 		0,  				WWDG_IRQn,  	0,			"",					0,	 false,	0,	0  	},
@@ -100,6 +100,31 @@ void 					enableInputs( bool fromThread ){							// check for any unprocessed In
 	}
 	EXTI->IMR |= KeypadIMR;		// re-enable keypad EXTI interrupts
 }						   					
+
+/*
+ * Disables interrupts for keys, prior to sleeping, to limit the keys that can
+ * cause an interrupt. Expected usage is to disable all but House. Before sleeping
+ * it is a very good idea to disable the Bowl, because that will silently boot
+ * to DFU mode, likely causing confusion.
+ *
+ * enum KEYS_MASK keysToRemainEnabled: A mask of KEYS_MASKs for the keys NOT to be
+ *     disabled.
+ */
+void disableKeyInterrupts(enum KEYS_MASK keysToRemainEnabled) {
+    // iterate over the membrane switch (keypad) keys
+    for (KEY k = kHOME; k < kINVALID; k++) {
+        if (((1<<k)&keysToRemainEnabled) == 0) {
+            int pin = keydef[k].pin;
+            int pinBit = 1 << pin;      // bit mask for EXTI->IMR, RTSR, FTSR
+            EXTI->RTSR &= ~pinBit;      // Enable a rising trigger
+            EXTI->FTSR &= ~pinBit;      // Enable a falling trigger
+            EXTI->IMR &= ~pinBit;      // Configure the interrupt mask
+            KeypadIMR &= ~pinBit;
+        }
+    }
+    EXTI->IMR &= ~KeypadIMR;		// disable keypad EXTI interrupts for the specified keys
+}
+
 
 /*bool 					updateKeyState( KEY k ){		// read keypad pin, update keydef[k] if changed, & generate osEvt if UP transition
 	bool keydown = gGet( keydef[k].id ); 
@@ -378,10 +403,10 @@ void 					inputThread( void *arg ){			// converts TB_Key msgs from keypad ISR's 
 		KEY k = transition->k;
 		uint32_t ts = transition->tstamp;
 		bool keydown = transition->down;
-		osMemoryPoolFree( KeyTransition_pool, transition );		
+		osMemoryPoolFree( KeyTransition_pool, transition );
 
-		dbgLog( "A keyTr: %c%c %d %dDn 1:%c 2:%c %c%c \n", 
-                keyNm[k], keydown? 'v':'^', ts, KSt.downCnt, keyNm[KSt.firstDown], keyNm[KSt.secondDown], KSt.multipleDown? '_':'M', KSt.starUsed? '_':'S'	);
+      dbgLog( "A keyTr: %c%c %d %dDn 1:%c 2:%c %c%c \n",
+              keyNm[k], keydown? 'v':'^', ts, KSt.downCnt, keyNm[KSt.firstDown], keyNm[KSt.secondDown], KSt.multipleDown? '_':'M', KSt.starUsed? '_':'S'	);
 		if ( (KSt.keyState[kPOT]!='_') && (KSt.keyState[kHOME]!='_')){
 			KeypadTestActive = !KeypadTestActive;
 			dbgLog("A KeypadTestMode %s \n", KeypadTestActive? "on":"off" );
@@ -583,6 +608,10 @@ void 					sendEvent( Event key, int32_t arg ){	// log & send TB_Event to CSM
 	if ( key==eNull || key==anyKey || key==eUNDEF || (int)key<0 || (int)key>(int)eUNDEF )
 		tbErr( "bad event" );
 	if (TBEvent_pool==NULL) return; //DEBUG
+  if (!controlManagerReady) {
+      dbgLog("enqueue event, csm not ready");
+      return;
+  }
 	dbgEvt( TB_keyEvt, key, arg, 0, 0 );
 	dbgLog( "A Evt: %s %d \n", eventNm(key), arg );
 	
@@ -591,8 +620,12 @@ void 					sendEvent( Event key, int32_t arg ){	// log & send TB_Event to CSM
 	TB_Event *evt = (TB_Event *)osMemoryPoolAlloc( TBEvent_pool, osWaitForever );
 	evt->typ = key;
 	evt->arg = arg;
-	if ( osMessageQueuePut( osMsg_TBEvents, &evt, 0, 0 ) != osOK )	// Priority 0, no wait
-		tbErr( "failed to enQ tbEvent" );
+  osStatus_t result;
+    // send with Priority 0, no wait
+	if ( (result=osMessageQueuePut( osMsg_TBEvents, &evt, 0, 0 )) != osOK ) {
+      printf("failed to enQ tbEvent: %d \n", result);
+      tbErr("failed to enQ tbEvent");
+  }
 }
 void 					keyPadPowerUp( void ){					// re-initialize keypad GPIOs
 	initializeInterrupts();

Src/ledmanager.c
@@ -75,6 +75,13 @@ void								seqAddShade( ledSeq *seq, ledShade shade, int ms ){				// add ms of 
 	seq->msec[ step ] = ms;
 	seq->nSteps = step+1;
 }
+
+/*
+ * Compile an LED string into a "ledSeq".
+ *
+ * seq:     Pointer to an "ledSeq" object to be filled.
+ * def:     The string to convert. If the string is null or an empty string, simply clears *seq.
+ */
 void								convertSeq( ledSeq *seq, const char *def ){		// fill 'seq' based on def e.g. "R20G100_10"
 	seq->nextStep = 0;
 	seq->shadeStep = 0;
@@ -201,6 +208,12 @@ void								ledThread( void *arg ){
 	}
 }
 //PUBLIC
+
+/*
+ * Sets the "foreground" LED sequence. Displays until cleared.
+ *
+ * def: A string describing the LEDs to show. See convertSeq.
+ */
 void 								ledFg( const char *def ){						// install 'def' as foreground pattern
 	if ( def==NULL || def[0]==0 ){  	// "" => switch to background pattern
 		currSeq = bgSeq;
@@ -215,6 +228,12 @@ void 								ledFg( const char *def ){						// install 'def' as foreground patte
 		osEventFlagsSet( osFlag_LedThr, LED_EVT );		// wakeup for ledThread
 	}
 }
+
+/*
+ * Sets the "background" LED sequence. Until cleared, displays when there is no foreground sequence.
+ *
+ * def: A string describing the LEDs to show. See convertSeq.
+ */
 void								ledBg( const char *def ){						// install 'def' as background pattern
 	dbgLog( "9 ledBg: %s \n", def );
 	if (def==NULL)	// NULL = turn off
@@ -231,9 +250,16 @@ void								ledBg( const char *def ){						// install 'def' as background patter
 		osEventFlagsSet( osFlag_LedThr, LED_EVT );		// wakeup ledThread
 	}
 }
+
+/*
+ * Enable or disable the LedManager.
+ *
+ * bool enable: If true, enable the LED manager.
+ */
 void								EnableLedMngr( bool enable ){
 	enableLedManager = enable;
 }
+
 void 								initLedManager(){				// initialize & spawn LED thread
 	handlePowerEvent( POWER_UP );
 //	registerPowerEventHandler( handlePowerEvent );

commit ece50e4d1caee9434003287c6e78818934da03ee

Fix a bug introduced in c5b36071.
A 'sendEvent()' call was moved inside an 'if', breaking the QC tests.

Src/audio.c
@@ -578,7 +578,7 @@ void								audLoadBuffs(){																// called on mediaThread to preload a
 extern bool BootVerboseLog;
 
 void 								audPlaybackComplete( void ) {									// shut down after completed playback
-  int pct;
+  int pct ;
   haltPlayback();     // updates tsPause & msPlayed
   audPlayDone();
   #ifdef _SQUARE_WAVE_SIMULATOR
@@ -605,9 +605,9 @@ void 								audPlaybackComplete( void ) {									// shut down after completed 
       
       // playback completed, report difference between measured & expected elapsed time
       if ( BootVerboseLog ) logEvtNI("Play_Tim", "dif_ms", pSt.msecLength - pSt.msPlayed );
-      sendEvent( AudioDone, pct );				// end of file playback-- generate CSM event
       pSt.stats->Finish++;
   }
+  sendEvent( AudioDone, pct );				// end of file playback-- generate CSM event
 }
 
 